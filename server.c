82f0c6f92122db01bd9a4474305559b86649a956
lukefreeland14 <luke_freeland@mines.edu>
2025-12-11 19:50:37 -0600

    Update server.c

--- server.c
+++ server.c
@@ -6,52 +6,46 @@
 #include <string.h>
 #include <signal.h>
 
-struct message {
+struct message
+{
 	char source[50];
-	char target[50]; 
+	char target[50];
 	char msg[200]; // message body
 };
 
-void terminate(int sig) {
+void terminate(int sig)
+{
 	printf("Exiting....\n");
 	fflush(stdout);
 	exit(0);
 }
 
-int main() {
+int main()
+{
 	int server;
 	int target;
 	int dummyfd;
 	struct message req;
-	signal(SIGPIPE,SIG_IGN);
-	signal(SIGINT,terminate);
-	server = open("serverFIFO",O_RDONLY);
-	dummyfd = open("serverFIFO",O_WRONLY);
+	signal(SIGPIPE, SIG_IGN);
+	signal(SIGINT, terminate);
+	server = open("serverFIFO", O_RDONLY);
+	dummyfd = open("serverFIFO", O_WRONLY);
 
-	while (1) {
-		// TODO:
+	while (1)
+	{
 		// read requests from serverFIFO
+		if (read(server, &req, sizeof(struct message)) != sizeof(struct message))
+			continue;
 
+		printf("Received a request from %s to send the message %s to %s.\n", req.source, req.msg, req.target);
 
-
-
-
-
-		printf("Received a request from %s to send the message %s to %s.\n",req.source,req.msg,req.target);
-
-		// TODO:
 		// open target FIFO and write the whole message struct to the target FIFO
 		// close target FIFO after writing the message
-
-
-
-
-
-
-
+		target = open(req.target, O_WRONLY);
+		write(target, &req, sizeof(struct message));
+		close(target);
 	}
 	close(server);
 	close(dummyfd);
 	return 0;
 }
-
